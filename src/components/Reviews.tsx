4363a7272c40ae4f77c15c06b24aa4d82b6bb067
miulerbm <miulerbm00@gmail.com>
2024-05-09 22:01:48 -0500

    Animated landing page section

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Phone from "./Phone";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+
+  return result;
+}
+
+function ReviewColumn({
+  reviews,
+  className,
+  reviewClassName,
+  msPerPixel = 0,
+}: {
+  reviews: string[];
+  className?: string;
+  reviewClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}) {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  const [columnHeight, setColumnHeight] = useState(0);
+  // the duration of the animation will depend on the size of the space we have available for showing the images. If there is a small space, we can´t make it go fast.
+  const duration = `${columnHeight * msPerPixel}ms`;
+
+  // The user can resize the window, so we need an useEffect to listen the resize event and set the column height accordingly
+
+  useEffect(() => {
+    if (!columnRef.current) return;
+
+    // builtin api that listens to resizing
+    // we can have a logic within a callback function that is called when that event happens (when the size changes)
+    const resizeObserver = new window.ResizeObserver(() => {
+      setColumnHeight(columnRef.current?.offsetHeight ?? 0);
+    });
+
+    // listen to changes on the div:
+    resizeObserver.observe(columnRef.current);
+
+    return () => {
+      //clean the observer after finishing its use.
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={columnRef}
+      className={cn("animate-marquee space-y-8 py-4", className)}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
+    >
+      {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
+        <Review
+          key={reviewIndex}
+          className={reviewClassName?.(reviewIndex % reviews.length)}
+          imgSrc={imgSrc}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+}
+
+function Review({ imgSrc, className, ...props }: ReviewProps) {
+  //randomize how fast each image goes
+  const POSSIBLE_ANIMATION_DELAYS = [
+    "0s",
+    "0.1s",
+    "0.2s",
+    "0.3s",
+    "0.4s",
+    "0.5s",
+  ];
+
+  const animationDelay =
+    POSSIBLE_ANIMATION_DELAYS[
+      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+    ];
+
+  return (
+    <div
+      className={cn(
+        "animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
+        className
+      )}
+      style={{ animationDelay }}
+      {...props}
+    >
+      <Phone imgSrc={imgSrc} />
+    </div>
+  );
+}
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  // This ref is for animating the grid only when is in the view part (when the user sees it)
+  // to check if the item is visible, we use the ref and the framer motion library
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  // we only want to animate the first time the user sees it. Kind of in the view (40%, not 100% is necessary)
+  const columns = splitArray(PHONES, 3);
+  const column1 = columns[0];
+  const column2 = columns[1];
+  const column3 = splitArray(columns[2], 2);
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? (
+        <>
+          <ReviewColumn
+            reviews={[...column1, ...column3.flat(), ...column2]}
+            reviewClassName={(reviewIndex) =>
+              cn({
+                "md:hidden": reviewIndex >= column1.length + column3[0].length,
+                "lg:hidden": reviewIndex >= column1.length,
+              })
+            }
+            msPerPixel={10}
+          />
+          <ReviewColumn
+            reviews={[...column2, ...column3[1]]}
+            className="hidden md:block"
+            reviewClassName={(reviewIndex) =>
+              reviewIndex >= column2.length ? "lg:hidden" : ""
+            }
+            msPerPixel={15}
+          />
+          <ReviewColumn
+            reviews={[...column3.flat()]}
+            className="hidden md:block"
+            msPerPixel={10}
+          />
+        </>
+      ) : null}
+      <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100"></div>
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100"></div>
+    </div>
+  );
+}
+
+export function Reviews() {
+  return (
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        className="absolute select-none hideen xl:block -left-32 top-1/3"
+      />
+      <ReviewGrid />
+    </MaxWidthWrapper>
+  );
+}
