11d2501285c8c0b47968f8f409c3be650fa654d9
miulerbm <miulerbm00@gmail.com>
2024-05-16 10:44:54 -0500

    Fixing styles and adding the auth-callback in env

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -19,7 +19,7 @@ const LoginModal = ({
 }) => {
   return (
     <Dialog onOpenChange={setIsOpen} open={isOpen}>
-      <DialogContent className="absolute z-[9999999]">
+      <DialogContent>
         <DialogHeader>
           <div className="relative mx-auto w-24 h-24 mb-2">
             <Image
